437f49e77aba941b33457bf30a245addfb0ac37f
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-08-24 14:41:01 +0200

    LIV. BASE: Creazione delle variabili km ed anni

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+*/ 
+
+// Creazione delle variabili che conterranno i km e gli anni acquisiti in input
+let kilometri = prompt("Inserire il numero di chilometri che si vuole percorrere oggi:");
+let anni = prompt("Inserire il numero di anni:");
+
+
+
